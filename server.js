1dc3d1a4653a48ecd21b136fbf57d543eabc9d31
HenningQvist <henning.qvist@hotmail.com>
2025-08-23 06:07:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ import jobsRouter from "./routes/jobs.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-
 // API routes
 app.use("/jobs", jobsRouter);
 
-app.listen(port, () => {
-  console.log(`✅ Servern kör på http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ Servern kör på port ${PORT}`);
 });
